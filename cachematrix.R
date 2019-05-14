31555eea88e9f49140d876409d4847daf97f2600
tianawangty <tianawangty@163.com>
2019-05-14 16:58:08 -0400

    Assignment 2 Filled In

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## m - inverse matrix value
+        m <- NULL
+        ## set: matrix and reset m
+        set <- function(y) {
+        ## set the value of the matrix and clear the value of inverse matrix
+        ## The "<<-" operator used to set variable that already exists 
+        ## in the parent environment.
+                x <<- y
+                m <<- NULL
+        }
+        ## get: get matrix
+        get <- function() x
+        ## Set inverse matrix
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
@@ -12,4 +29,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        ## Check if it is not null
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        ## If it is null, calculate the inverse
+        data <- x$get() # get the matrix to inverse
+        m <- solve(data, ...) # inverse matrix
+        x$setinverse(m) # store the result in makeCacheMatrix
+        m # return inverse matrix
 }
